e31bee6dc08a443df58ea1f7ff0b99038400190d
Mustafa ÇOK <96659190+Mustafacok@users.noreply.github.com>
2023-03-18 02:48:56 +0300

    Add project files.

---
+++ AuthSystemUserRegistration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthSystemUserRegistration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
